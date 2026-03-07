53170d92e784d64a081ce4d10c8972711ab6d421
szabopeter <szabopeter0426@gmail.com>
2026-03-08 00:58:50 +0100

    aaasdasdasd

--- src/components/canvas/CanvasScene.jsx
+++ src/components/canvas/CanvasScene.jsx
@@ -1,13 +1,27 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { Stage, ScrollControls } from "@react-three/drei";
 import { Model } from "./Fejhallgatoo";
 import ScrollManager from "./ScrollManager";
 import { Environment, ContactShadows } from "@react-three/drei";
 
 export default function CanvasScene({ onReady }) {
   return (
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [1, 2.5, 2], fov: 45 }} className="canvas-container">
+    <Canvas
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [1, 2.5, 2], fov: 45 }}
+      className="canvas-container"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        height: "100svh",
+        width: "100vw",
+        zIndex: 10,
+        pointerEvents: "none",
+      }}
+      resize={{ scroll: false, debounce: { scroll: 50, resize: 0 } }}
+    >
       <ScrollManager />
       <Suspense fallback={null}>
         <Environment preset="city" environmentIntensity={0.7} adjustCamera={false} />
