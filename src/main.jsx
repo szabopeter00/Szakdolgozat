c315b3c56cf7e9a0cafd977fe13a042c76e22c01
szabopeter <szabopeter0426@gmail.com>
2026-04-05 00:16:50 +0200

    trukkozgetesek kozepette

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+setTimeout(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}, 0);
