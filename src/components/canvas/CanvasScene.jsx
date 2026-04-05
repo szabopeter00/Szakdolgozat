8cb22c9f87230696c45383db538673dc6f16c260
szabopeter <szabopeter0426@gmail.com>
2026-04-05 14:39:50 +0200

    teszt-3

--- src/components/canvas/CanvasScene.jsx
+++ src/components/canvas/CanvasScene.jsx
@@ -16,7 +16,7 @@ export default function CanvasScene({ onReady }) {
         top: 0,
         left: 0,
         width: "100%",
-        height: "100dvh",
+        height: "100vh",
         zIndex: 10,
         pointerEvents: "none",
       }}
